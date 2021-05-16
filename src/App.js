044534b0932991e004b91324c3701591e02ab5b1
Rajko Culibrk <rajko93culibrk@gmail.com>
2021-05-16 15:39:43 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComponent from "./components/core/NavbarComponent";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComponent />
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductCard from "../components/Product/ProductCard";
+
+const Home = () => {
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
+};
+
+export default Home;
